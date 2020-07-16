5efe48f376007dd04f4e262baa6a7c5d2007e38a
Yurii Khoroshchak <khoroshchak.yu@gmail.com>
2020-07-16 10:39:18 +0300

    finish

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -39,9 +39,41 @@ const GithubState = (props) => {
 
   //Get user
 
+  const getUser = async (username) => {
+    setLoading(true);
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_USER,
+      payload: res.data,
+    });
+  };
+
   //Get repos
 
+  const getUserRepos = async (username) => {
+    setLoading(true);
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    });
+  };
+
   //Clear users
+
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
   //Set loading
 
@@ -55,6 +87,9 @@ const GithubState = (props) => {
         repos: state.repos,
         loading: state.loading,
         searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos,
       }}
     >
       {props.children}
